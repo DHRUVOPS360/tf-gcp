9486ab7830346f5d58e74f6c9a501158bed9e775
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-20 16:10:42 +0530

    Create a bucket and a compute instance.

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*resource "google_storage_bucket" "auto-expire" {
+resource "google_storage_bucket" "auto-expire" {
   name          = "engineer-cloud-nprod-bucket"
   location      = "US"
   force_destroy = true
@@ -21,4 +21,45 @@
     }
   }
 }
-*/
+
+resource "google_compute_instance" "default" {
+  name         = "engineer-cloud-nprod-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
