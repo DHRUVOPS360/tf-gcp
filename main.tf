5880392a1cc0f189bc02b5869e72115b0e6735b7
DRUVANAND V <105794572+DRUVANAND@users.noreply.github.com>
2023-03-20 16:35:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "google_compute_instance" "default" {
   }
 
   network_interface {
-    network = "default"
+    network = "dhruv-vpc"
 
     access_config {
       // Ephemeral public IP
